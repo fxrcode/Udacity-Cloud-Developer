9439770386f116f5cab79ea36b4494e54bdcefa7
fxrcode <fxrcode@gmail.com>
2021-03-01 13:04:26 -0800

    U3: project 2 local tested using postman

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { Request, Response } from 'express';
 
 (async () => {
 
@@ -9,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +31,41 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    const url = req.query.image_url;
+    // console.log(url);
+    // 1. validate the image_url query, not none
+    if (!url) {
+      return res.status(400).send({ message: 'no valid image_url is given!' });
+    }
+
+    try {
+      // 2. filterImageFromURL(image_url), get the absolute filtered image path
+      const filteredpath = await filterImageFromURL(url);
+      // 3. send the resulting local file in response
+      return res.status(201).sendFile(filteredpath, async (error) => {
+        if (error) {
+          throw error;
+        }
+        // console.log(filteredpath);
+        // 4. deletes any files on the server on finish of the response
+        await deleteLocalFiles([filteredpath]);
+      })
+
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
