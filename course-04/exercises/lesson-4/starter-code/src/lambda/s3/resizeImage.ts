03675ac3a5beeeef9479425105f373b6566214e8
fxrcode <fxrcode@gmail.com>
2021-03-17 21:19:41 -0700

    c4-lesson4-starter: add an event sources for the ResizeImage lambda

--- course-04/exercises/lesson-4/starter-code/src/lambda/s3/resizeImage.ts
+++ course-04/exercises/lesson-4/starter-code/src/lambda/s3/resizeImage.ts
@@ -1,4 +1,4 @@
-import { SNSEvent, SNSHandler } from 'aws-lambda'
+import { SNSEvent, SNSHandler, S3EventRecord } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 import Jimp from 'jimp/es'
