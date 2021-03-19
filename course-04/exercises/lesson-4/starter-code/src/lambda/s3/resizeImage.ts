1dc7e13196317322bb43c12a0487c8f6070336be
fxrcode <fxrcode@gmail.com>
2021-03-18 22:40:03 -0700

    testing c4-lesson4-starter

--- course-04/exercises/lesson-4/starter-code/src/lambda/s3/resizeImage.ts
+++ course-04/exercises/lesson-4/starter-code/src/lambda/s3/resizeImage.ts
@@ -9,4 +9,50 @@ const imagesBucketName = process.env.IMAGES_S3_BUCKET
 const thumbnailBucketName = process.env.THUMBNAILS_S3_BUCKET
 
 export const handler: SNSHandler = async (event: SNSEvent) => {
+    console.log('Processing SNS event ' , JSON.stringify(event))
+    for (const snsRecord of event.Records ) {
+        const s3EventStr = snsRecord.Sns.Message
+        console.log('Processing S3 event', s3EventStr)
+        const s3Event = JSON.parse(s3EventStr)
+
+        for (const record of s3Event.Records) {
+            await processImage(record) // copied from sendNotifications.ts
+        }
+    }
+}
+
+async function processImage(record: S3EventRecord) {
+    // get a key of an uploaded image in S3
+    const key = record.s3.object.key
+    console.log('Procesing S3 item with key: ', key)
+
+    // download the image with key
+    const response = await s3
+        .getObject({
+            Bucket: imagesBucketName,
+            Key: key
+        })
+        .promise()
+
+    const body = response.Body
+
+    // resize the image
+    //  1. read an image with JImp library
+    const image = await Jimp.read(body)
+
+    console.log('Resizing image')
+    //  2. resize an image
+    image.resize(150, Jimp.AUTO)
+    //  3. convert the thumbtail to a buffer so as to upload to another S3
+    const convertedBuffer = await image.getBufferAsync(Jimp.AUTO)
+
+    console.log(`Writing thumtail to another S3: ${thumbnailBucketName}`)
+    // put the thumbtail image into another S3
+    await s3
+        .putObject({
+            Bucket: thumbnailBucketName,
+            Key: `${key}.jpeg`,
+            Body: convertedBuffer
+        })
+        .promise()
 }
